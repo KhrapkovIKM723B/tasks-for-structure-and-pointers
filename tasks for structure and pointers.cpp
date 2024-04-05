9a58f0507b88c1c511576002319bd72b41c6c498
KhrapkovIKM723B <Ihor.Khrapkov@infiz.khpi.edu.ua>
2024-04-06 00:05:51 +0300

    commit all

---
+++ tasks for structure and pointers.cpp
@@ -0,0 +1,54 @@
+﻿// tasks for structure and pointers.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+
+
+struct Student
+
+
+{
+	int years[4];
+	string surname[4];
+	float average[4];
+};
+
+int main()
+{
+	
+	Student Gorochko;
+	Gorochko.years = 2003;
+	Gorochko.average=75;
+
+
+
+
+
+
+
+	Student Golyb;
+	Golyb.years = 2002;
+	Golyb.average = 85;
+
+
+
+
+
+	
+	Student Golovco;
+	Golovco.years = 2002;
+	Golovco.average = 65;
+
+
+
+
+
+	Student Goys;
+	Goys.years = 2003;
+	Goys.average = 95;
+
+
+}
